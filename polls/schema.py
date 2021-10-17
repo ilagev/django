d9c6ad44c89454cadcc1965220b169eea1b71bdc
Iván Lage Vázquez <ilagev@gmail.com>
2021-10-17 19:25:28 +0200

    Adding creation mutation + stop using graphene_django

--- polls/schema.py
+++ polls/schema.py
@@ -1,28 +1,31 @@
 import graphene
+from django.utils import timezone
 from graphene.types import schema
-from graphene_django import DjangoObjectType, fields
+from graphene import ObjectType
+from graphene.types.field import Field
 from .models import Question, Choice
 
 # Types
 
-class QuestionsType(DjangoObjectType):
-    class Meta:
-        model = Question
-        fields = ('id', 'question_text', 'pub_date')
+class QuestionType(ObjectType):
+    id = graphene.ID()
+    question_text = graphene.String()
+    pub_date = graphene.Date()
 
-class ChoiceType(DjangoObjectType):
-    class Meta:
-        model = Choice
-        fields = ('id', 'choice_text', 'question', 'votes')
+class ChoiceType(ObjectType):
+    id = graphene.ID()
+    choice_text = graphene.String()
+    votes = graphene.Int()
+    question = graphene.Field(QuestionType)
 
 # Queries
 
 class Query(graphene.ObjectType):
-    all_questions = graphene.List(QuestionsType)
+    all_questions = graphene.List(QuestionType)
     all_choices = graphene.List(ChoiceType)
-    question = graphene.Field(QuestionsType, id=graphene.Int())
+    question = graphene.Field(QuestionType, id=graphene.Int())
     choices_of_question = graphene.List(ChoiceType, questionId=graphene.Int())
-    filtered_questions = graphene.List(QuestionsType, substring=graphene.String())
+    filtered_questions = graphene.List(QuestionType, substring=graphene.String())
 
     def resolve_all_questions(root, info):
         return Question.objects.all()   
@@ -38,5 +41,24 @@ class Query(graphene.ObjectType):
 
     def resolve_filtered_questions(root, info, substring):
         return Question.objects.filter(question_text__contains=substring)
+
+# Mutations
+
+class QuestionMutation(graphene.Mutation):
     
-schema = graphene.Schema(query=Query)
+    class Arguments:
+        text = graphene.String(required=True)
+
+    question = graphene.Field(QuestionType)
+
+    @classmethod
+    def mutate(cls, root, info, text):
+        question = Question(question_text=text)
+        question.pub_date = timezone.now()
+        question.save()
+        return QuestionMutation(question=question)
+
+class Mutation(graphene.ObjectType):
+    update_question = QuestionMutation.Field()
+    
+schema = graphene.Schema(query=Query, mutation=Mutation)
